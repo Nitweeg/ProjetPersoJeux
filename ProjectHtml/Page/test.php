ff8bebb07d205e2ddd4174cd4dfc3e64c7ae9139
Nitweeg <60885411+Nitweeg@users.noreply.github.com>
2022-04-04 18:50:18 +0200

    Add files via upload

---
+++ ProjectHtml/Page/test.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <meta charset="utf-8">
+    <title>test </title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    
+    <link rel="stylesheet" href="../style/test.css" type="text/css">
+    </head>
+<body>
+<div class="card">
+<div class="row no-gutters">
+	<aside class="col-md-9">
+			<article class="card-body border-bottom">
+					<div class="row">
+						<div class="col-md-7">
+							<figure class="itemside">
+								<div class="aside"><img src="bootstrap-ecommerce-html/images/items/1.jpg" class="border img-sm"></div>
+								<figcaption class="info">
+									<a href="#" class="title">Some name of item goes here nice </a>
+									<strong class="">$128.00</strong>
+									<div>
+										<a href="#" class="btn-link mr-2">Save for later</a> 
+										<a href="#" class="btn-link text-danger"> Delete</a>
+									</div>
+								</figcaption>
+							</figure> 
+						</div> <!-- col.// -->
+						<div class="col-md-5 text-md-right text-right"> 
+							<div class="input-group input-spinner">
+							  <div class="input-group-prepend">
+							    <button class="btn btn-light" type="button" id="button-plus"> <i class="fa fa-plus"></i> </button>
+							  </div>
+							  <input type="text" class="form-control" value="1">
+							  <div class="input-group-append">
+							    <button class="btn btn-light" type="button" id="button-minus"> <i class="fa fa-minus"></i> </button>
+							  </div>
+							</div> <!-- input-group.// -->
+						</div>
+					</div> <!-- row.// -->
+			</article> <!-- card-group.// -->
+			<article class="card-body border-bottom">
+					<div class="row">
+						<div class="col-md-7">
+							<figure class="itemside">
+								<div class="aside"><img src="bootstrap-ecommerce-html/images/items/2.jpg" class="border img-sm"></div>
+								<figcaption class="info">
+									<a href="#" class="title">Product name goes here nice </a>
+									<strong class="">$128.00</strong>
+									<div>
+										<a href="#" class="btn-link mr-2">Save for later</a> 
+										<a href="#" class="btn-link text-danger"> Delete</a>
+									</div>
+								</figcaption>
+							</figure> 
+						</div> <!-- col.// -->
+						<div class="col-md-5 text-md-right text-right"> 
+							<div class="input-group input-spinner">
+							  <div class="input-group-prepend">
+							    <button class="btn btn-light" type="button" id="button-plus"> <i class="fa fa-plus"></i> </button>
+							  </div>
+							  <input type="text" class="form-control" value="1">
+							  <div class="input-group-append">
+							    <button class="btn btn-light" type="button" id="button-minus"> <i class="fa fa-minus"></i> </button>
+							  </div>
+							</div> <!-- input-group.// -->
+						</div>
+					</div> <!-- row.// -->
+			</article> <!-- card-group.// -->
+			<article class="card-body border-bottom">
+					<div class="row">
+						<div class="col-md-7">
+							<figure class="itemside">
+								<div class="aside"><img src="bootstrap-ecommerce-html/images/items/3.jpg" class="border img-sm"></div>
+								<figcaption class="info">
+									<a href="#" class="title">Another name of some product goes just </a>
+									<strong class="">$98.50</strong>
+									<div>
+										<a href="#" class="btn-link mr-2">Save for later</a> 
+										<a href="#" class="btn-link text-danger"> Delete</a>
+									</div>
+								</figcaption>
+							</figure> 
+						</div> <!-- col.// -->
+						<div class="col-md-5 text-md-right text-right"> 
+							<div class="input-group input-spinner">
+							  <div class="input-group-prepend">
+							    <button class="btn btn-light" type="button" id="button-plus"> <i class="fa fa-plus"></i> </button>
+							  </div>
+							  <input type="text" class="form-control" value="1">
+							  <div class="input-group-append">
+							    <button class="btn btn-light" type="button" id="button-minus"> <i class="fa fa-minus"></i> </button>
+							  </div>
+							</div> <!-- input-group.// -->
+						</div>
+					</div> <!-- row.// -->
+			</article> <!-- card-group.// -->
+	</aside> <!-- col.// -->
+	<aside class="col-md-3 border-left">
+		<div class="card-body">
+			<dl class="dlist-align">
+			  <dt>Total price:</dt>
+			  <dd class="text-right">$69.00</dd>
+			</dl>
+			<dl class="dlist-align">
+			  <dt>Discount:</dt>
+			  <dd class="text-right text-danger">- $13.00</dd>
+			</dl>
+			<dl class="dlist-align">
+			  <dt>Total:</dt>
+			  <dd class="text-right text-dark b"><strong>$80.45</strong></dd>
+			</dl>
+			<hr>
+			<a href="#" class="btn btn-primary btn-block"> Make Purchase </a>
+			<a href="#" class="btn btn-light btn-block">Continue Shopping</a>
+		</div> <!-- card-body.// -->
+	</aside> <!-- col.// -->
+</div> <!-- row.// -->
+</div>
+</body>
+</html>
